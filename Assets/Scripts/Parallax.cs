ac5f111f1203784dee21c92cce27c10c57ab2cad
yoVeronn <ban.veronike@gmail.com>
2021-07-08 16:57:22 +0800

    Update 1.6
    - Background repeat fixed (added more iterations, took note of sprite length)
    - changed camera angle
    - light effect needs realism

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,27 +5,30 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, startPos;
-    public GameObject cam;
+    public GameObject referencePoint;
     public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        length = (GetComponent<SpriteRenderer>().bounds.size.x); //1055
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (referencePoint.transform.position.x * (1 - parallaxEffect));
+        float dist = (referencePoint.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
 
         if (temp > (startPos + length))
         {
             startPos += length;
         }
-        else if (temp < (startPos - length)) startPos -= length;
+        else if (temp < (startPos - length))
+        {
+            startPos -= length;
+        }
     }
 }
